60f1bb1c5dcb967de06eec1ac4852e050bac497f
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2017-12-25 18:43:29 -0500

    autocommit 0 for the ethnography paper

--- docs/diff2.js
+++ docs/diff2.js
@@ -8,6 +8,7 @@ var child;
 var child2;
 var child3;
 var child4;
+var child5;
 
 
 const delay = require('delay');
@@ -21,31 +22,37 @@ var files = [
 
  watch('index.html', { recursive: true }, function(evt, name) {
   console.log('%s changed.', name);
+
+child5 = exec("ls -lR /Users/mp/thst6329/docs/*.html | wc -l", function (error, stdout, stderr)
 {
-myFuncCalls++;
+	console.log(stdout + " number of files")
+});
 
-}
-  console.log(myFuncCalls);
+//{myFuncCalls++;}
+//console.log(myFuncCalls);
 
+// output current state of git to a .diff file
 child = exec("git diff -U$(wc -l /Users/mp/thst6329/docs/index.html | xargs) > ethnography.diff")
+
+//convert the .diff file to html using diff2html
 child2 = exec("diff2html -i file ethnography.diff -F page" + myFuncCalls + ".html -- -M HEAD~1");
         // Executed after 200 milliseconds 
 
-
+//couldn't get the 'promise' to work, so using just a delay, add the unstaged changes
 delay(500)
-    
+   
     .then(() => {
     	child3 = exec("git add .");
     	
     });
 
 
-
-	delay(1000)
+//wait until end, then autocommit!
+delay(1000)
     
-    .then(() => {
-    	child4 = exec("git commit -m \"autocommit " + myFuncCalls + " for the ethnography paper\"");
-    	console.log("autocommit " + myFuncCalls + " for the ethnography paper")
+.then(() => {
+	child4 = exec("git commit -m \"autocommit " + myFuncCalls + " for the ethnography paper\"");
+    console.log("autocommit " + myFuncCalls + " for the ethnography paper")
 
         // Executed after 200 milliseconds 
     });
