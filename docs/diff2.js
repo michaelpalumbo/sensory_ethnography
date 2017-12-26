9e4044155535117baca29245bc4ad0619d34163a
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2017-12-26 12:29:33 -0500

    first commit using the index html logger

--- docs/diff2.js
+++ docs/diff2.js
@@ -1,6 +1,7 @@
 var watch = require('node-watch');
 
-var myFuncCalls = 0;
+var count = 0; //this is the total number of .html files already in the folder. 
+
 
 var util = require('util')
 var exec = require('child_process').exec;
@@ -10,7 +11,6 @@ var child3;
 var child4;
 var child5;
 
-var count; //this is the total number of .html files already in the folder. 
 
 
 const delay = require('delay');
@@ -25,19 +25,20 @@ var files = [
  watch('index.html', { recursive: true }, function(evt, name) {
   console.log('%s changed.', name);
 
-child5 = exec("ls -lR /Users/mp/thst6329/docs/*.html | wc -l", function (error, stdout, stderr)
-{ count = stdout
-	console.log(count) //need to make this global...
-});
+//child5 = exec("ls -lR /Users/mp/thst6329/docs/*.html | wc -l", function (error, stdout, stderr)
+//{ count = stdout
+//	console.log(count) //need to make this global...
+//});
 
-//{myFuncCalls++;}
-//console.log(myFuncCalls);
+
+{count++;}
 
 // output current state of git to a .diff file
 child = exec("git diff -U$(wc -l /Users/mp/thst6329/docs/index.html | xargs) > ethnography.diff")
 
 //convert the .diff file to html using diff2html
 child2 = exec("diff2html -i file ethnography.diff -F page" + count + ".html -- -M HEAD~1");
+console.log("page" + count + ".html")
         // Executed after 200 milliseconds 
 
 //couldn't get the 'promise' to work, so using just a delay, add the unstaged changes
@@ -53,7 +54,9 @@ delay(500)
 delay(1000)
     
 .then(() => {
-	child4 = exec("git commit -m \"autocommit " + count + " for the ethnography paper\"");
+	child4 = exec('git commit -m "autocommit " + count + " for the ethnography paper"');
+    console.log("autocommit " + count + " for the ethnography paper")
+
         // Executed after 200 milliseconds 
     });
 
